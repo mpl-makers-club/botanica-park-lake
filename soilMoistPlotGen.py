b66dea9a4cfcad9bfbab86b49cbcfa7fea9cc151
Edmond <61327779+edmond0167@users.noreply.github.com>
2021-03-15 22:12:48 +1100

    now

--- soilMoistPlotGen.py
+++ soilMoistPlotGen.py
@@ -9,10 +9,24 @@ print(df)
 fig = go.Figure()
 
 fig.add_trace(go.Scatter(x=df.date, y=df['soil'], name="Soil",
+                         marker=dict(
+                             color="LightSkyBlue",
+                             size=20,
+                             line=dict(
+                                 color="mediumPurple",
+                                 width=2
+                            )),
                          line_color='deepskyblue'))
 
-fig.update_layout(title_text='Botanica Park Soil Moisture Sensor',
-                  xaxis_rangeslider_visible=False)
+fig.update_layout(
+    title_text='Botanica Park Soil Moisture Sensor',
+    xaxis_title="Time",
+    yaxis_title="Moisture",
+    font=dict(
+        size=26,
+        color="RebeccaPurple"
+        ),
+    xaxis_rangeslider_visible=False)
 
 
 plotly.offline.plot(fig,
